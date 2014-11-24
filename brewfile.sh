af770352562ef79a5dc357d0897fd524735ea621
Mo Lawson <moklawson@gmail.com>
2014-11-24 10:18:12 -0600

    Replace Brewfile with plain bash equivalent

---
+++ brewfile.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+brew tap thoughtbot/formulae
+
+brew install ctags
+brew install elixir
+brew install git
+brew install gitsh
+brew install go
+brew install memcached
+brew install mongodb
+brew install mysql
+brew install nginx
+brew install node
+brew install openssl
+brew install phantomjs
+brew install postgresql
+brew install reattach-to-user-namespace
+brew install rbenv
+brew install rbenv-aliases
+brew install rbenv-default-gems
+brew install rbenv-gem-rehash
+brew install rbenv-vars
+brew install redis
+brew install ruby-build
+brew install ssh-copy-id
+brew install the_silver_searcher
+brew install tmux
+brew install vim
