9f68de4f0f766477121a2f2a1caf632f3d707645
Mo Lawson <mo@molawson.com>
2022-11-01 15:20:45 -0500

    Auto format hammerspoon configs

--- hammerspoon/browser.lua
+++ hammerspoon/browser.lua
@@ -1,75 +1,73 @@
 -- Copied from @danielma/dotfiles
-
 browsers = {
-   { "Safari", "com.apple.Safari" },
-   { "Firefox", "org.mozilla.firefox" },
-   { "Google Chrome", "com.google.chrome" },
-   { "Brave Browser", "com.brave.browser" },
-   { "Microsoft Edge", "com.microsoft.edgemac" }
+    {"Safari", "com.apple.Safari"}, {"Firefox", "org.mozilla.firefox"},
+    {"Google Chrome", "com.google.chrome"},
+    {"Brave Browser", "com.brave.browser"},
+    {"Microsoft Edge", "com.microsoft.edgemac"}
 }
 lastBrowser = "Safari"
 
-function launchBrowser()
-   launch(lastBrowser)
-end
+function launchBrowser() launch(lastBrowser) end
 
 function browserEvent(appName, eventType, app)
-   if eventType ~= hs.application.watcher.activated then
-      return
-   end
+    if eventType ~= hs.application.watcher.activated then return end
 
-   for _, browser in ipairs(browsers) do
-      if browser[1] == appName then
-         if appName ~= lastBrowser then
-            print("changing browser from " .. lastBrowser .. " to " .. appName)
-            lastBrowser = appName
-         end
+    for _, browser in ipairs(browsers) do
+        if browser[1] == appName then
+            if appName ~= lastBrowser then
+                print("changing browser from " .. lastBrowser .. " to " ..
+                          appName)
+                lastBrowser = appName
+            end
 
-         break
-      end
-   end
+            break
+        end
+    end
 end
 
 browserWatcher = hs.application.watcher.new(browserEvent)
 browserWatcher:start()
 
 function httpCallback(scheme, _, _, fullURL)
-   local spotifyRegex = "^https://open.spotify.com/"
-   local notionRegex = "^https://.*notion.so/"
-   local threadsRegex = "^https://threads.com/"
-   local trelloRegex = "^https://trello.com/"
-   local zoomRegex = "^https://.*zoom.us/j/(%d+)"
-   local zoomWithPasswordRegex = "^https://.*zoom.us/j/(%d+)?pwd=(%w+)"
+    local spotifyRegex = "^https://open.spotify.com/"
+    local notionRegex = "^https://.*notion.so/"
+    local threadsRegex = "^https://threads.com/"
+    local trelloRegex = "^https://trello.com/"
+    local zoomRegex = "^https://.*zoom.us/j/(%d+)"
+    local zoomWithPasswordRegex = "^https://.*zoom.us/j/(%d+)?pwd=(%w+)"
+    local asanaRegex = "^https://app.asana.com/"
 
-   if fullURL:match(spotifyRegex) then
-      fullURL = fullURL:gsub(spotifyRegex, "spotify://")
-      hs.urlevent.openURL(fullURL)
-   elseif fullURL:match(notionRegex) then
-      fullURL = fullURL:gsub(notionRegex, "notion://")
-      hs.urlevent.openURL(fullURL)
-   elseif fullURL:match(threadsRegex) then
-      fullURL = fullURL:gsub(threadsRegex, "threads://")
-      hs.urlevent.openURL(fullURL)
-   elseif fullURL:match(trelloRegex) then
-      fullURL = fullURL:gsub(trelloRegex, "trello://trello.com/")
-      hs.urlevent.openURL(fullURL)
-   elseif fullURL:match(zoomWithPasswordRegex) then
-      fullURL = fullURL:gsub(zoomWithPasswordRegex, "zoommtg://zoom.us/join?confno=%1&pwd=%2")
-      hs.urlevent.openURL(fullURL)
-   elseif fullURL:match(zoomRegex) then
-      fullURL = fullURL:gsub(zoomRegex, "zoommtg://zoom.us/join?confno=%1")
-      hs.urlevent.openURL(fullURL)
-   else
-      for _, browser in ipairs(browsers) do
-         if browser[1] == lastBrowser then
-            local frontmostApp = hs.application.frontmostApplication()
-            print(frontmostApp:bundleID())
-            local isHammerspoon = frontmostApp:bundleID() == "org.hammerspoon.Hammerspoon"
-            print(hs.urlevent.openURLWithBundle(fullURL, browser[2]))
-            break
-         end
-      end
-   end
+    if fullURL:match(spotifyRegex) then
+        fullURL = fullURL:gsub(spotifyRegex, "spotify://")
+        hs.urlevent.openURL(fullURL)
+    elseif fullURL:match(notionRegex) then
+        fullURL = fullURL:gsub(notionRegex, "notion://")
+        hs.urlevent.openURL(fullURL)
+    elseif fullURL:match(threadsRegex) then
+        fullURL = fullURL:gsub(threadsRegex, "threads://")
+        hs.urlevent.openURL(fullURL)
+    elseif fullURL:match(trelloRegex) then
+        fullURL = fullURL:gsub(trelloRegex, "trello://trello.com/")
+        hs.urlevent.openURL(fullURL)
+    elseif fullURL:match(zoomWithPasswordRegex) then
+        fullURL = fullURL:gsub(zoomWithPasswordRegex,
+                               "zoommtg://zoom.us/join?confno=%1&pwd=%2")
+        hs.urlevent.openURL(fullURL)
+    elseif fullURL:match(zoomRegex) then
+        fullURL = fullURL:gsub(zoomRegex, "zoommtg://zoom.us/join?confno=%1")
+        hs.urlevent.openURL(fullURL)
+    else
+        for _, browser in ipairs(browsers) do
+            if browser[1] == lastBrowser then
+                local frontmostApp = hs.application.frontmostApplication()
+                print(frontmostApp:bundleID())
+                local isHammerspoon = frontmostApp:bundleID() ==
+                                          "org.hammerspoon.Hammerspoon"
+                print(hs.urlevent.openURLWithBundle(fullURL, browser[2]))
+                break
+            end
+        end
+    end
 end
 
 hs.urlevent.httpCallback = httpCallback
