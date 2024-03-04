dc84ae440768e287ad83151425225cd673ee569d
Mo Lawson <mo@molawson.com>
2024-03-04 10:12:10 -0600

    Add a few file utility functions for neovim

--- config/nvim/lua/fn/files.lua
+++ config/nvim/lua/fn/files.lua
@@ -13,4 +13,13 @@ function files.write_p()
     vim.cmd.write()
 end
 
+function files.exists(filename)
+    local stat = vim.loop.fs_stat(filename)
+    return stat and stat.type or false
+end
+
+function files.is_dir(filename) return files.exists(filename) == "directory" end
+
+function files.is_file(filename) return files.exists(filename) == "file" end
+
 return files
