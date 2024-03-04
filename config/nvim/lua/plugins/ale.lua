0b2aa5026b954c5b4872d391b43178a1c9c2afa9
Mo Lawson <mo@molawson.com>
2024-03-04 10:14:41 -0600

    Update ale options to match lspconfig
    Sometimes I'll use ale for linting, and other times I'll use various
    LSP plugins. These configs will make the ale setup look and behave more
    similarly to the LSP setup.

--- config/nvim/lua/plugins/ale.lua
+++ config/nvim/lua/plugins/ale.lua
@@ -10,4 +10,6 @@ vim.g.ale_fixers = {
 }
 vim.g.ale_linters = {ruby = {"rubocop", "srb", "standard"}}
 vim.g.ale_fix_on_save = 1
-vim.g.ale_set_highlights = 0
+vim.g.ale_echo_cursor = 0
+vim.g.ale_use_neovim_diagnostics_api = 1
+vim.g["airline#extensions#ale#enabled"] = 0
