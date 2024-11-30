6c48ae26e6750d9289e7a946744a10407ca97a49
Mo Lawson <mo@molawson.com>
2024-11-30 07:49:34 -0600

    Better paging options for FZF window

--- config/nvim/lua/plugins/fzf.lua
+++ config/nvim/lua/plugins/fzf.lua
@@ -1,2 +1,3 @@
 vim.env.FZF_DEFAULT_COMMAND = "ag -lU --hidden --nocolor -g ''"
-vim.env.FZF_DEFAULT_OPTS = "--layout=reverse"
+vim.env.FZF_DEFAULT_OPTS =
+    "--layout=reverse --bind alt-up:preview-half-page-up,alt-down:preview-half-page-down"
